61862c4f863c69c5af9713645faa0836691d4875
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-01-31 14:58:59 -0500

    `MotionMode` works like a charm now :)

--- src/MachinaGrasshopper/Actions/MotionMode.cs
+++ src/MachinaGrasshopper/Actions/MotionMode.cs
@@ -25,38 +25,57 @@ namespace MachinaGrasshopper
     public class MotionMode : GH_Component
     {
 
-        //private int mType = (int) MotionType.Linear;  // can we serialize this?
-        private bool linear = true;
+        // Not very programmatic, but need to get this out... 
+        private Dictionary<MotionType, bool> mtToggles = new Dictionary<MotionType, bool>()
+        {
+            { MotionType.Linear, false },
+            { MotionType.Joint, false }
+        };
 
-        private Dictionary<MotionType, bool> mtToggles = new Dictionary<MotionType, bool>();
-        private MotionType currentmode = MotionType.Linear;
+        private MotionType m_currentmm = MotionType.Linear;
+        protected MotionType CurrentMotionMode
+        {
+            get { return m_currentmm; }
+            set
+            {
+                // Uncheck old mode
+                mtToggles[m_currentmm] = false;
 
-        private bool[] toggles = new bool[Enum.GetNames(typeof(MotionType)).Length];
+                // Check new one
+                m_currentmm = value;
+                mtToggles[m_currentmm] = true;
 
+                // Update component look
+                this.Message = Enum.GetName(typeof(MotionType), m_currentmm);
+                this.ExpireSolution(true);
+            }
+        }
 
 
         public MotionMode() : base(
             "MotionMode",
             "MotionMode",
-            "Sets the current motion mode to be applied to future actions. This can be \"linear\" (default) for straight line movements in euclidean space, or \"joint\" for smooth interpolation between axes angles. NOTE: \"joint\" motion may produce unexpected trajectories resulting in reorientations or collisions; use with caution!",
+            "Sets the current motion mode to be applied to future actions. This can be \"linear\" (default) for straight line movements in euclidean space, or \"joint\" for smooth interpolation between axes angles. NOTE: \"joint\" motion may produce unexpected trajectories resulting in reorientations or collisions; use with caution! Right-click to set MotionMode.",
             "Machina",
             "Actions")
         { }
         public override GH_Exposure Exposure => GH_Exposure.secondary;
         public override Guid ComponentGuid => new Guid("b7681714-6734-474c-b5d5-6fb8003f674f");
-        protected override System.Drawing.Bitmap Icon => Properties.Resources.Actions_Attach;
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.Actions_MotionMode;
 
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             // There will be no input, mode changes from item menu
             //pManager.AddTextParameter("Type", "T", "\"linear\" or \"joint\"", GH_ParamAccess.item, "linear");
 
-            foreach (MotionType t in Enum.GetValues(typeof(MotionType)))
-            {
-                mtToggles.Add(t, t == currentmode);
-            }
+            //// Initialize dict programmatically
+            //foreach (MotionType t in Enum.GetValues(typeof(MotionType)))
+            //{
+            //    mtToggles.Add(t, t == currentmode);
+            //}
+            //this.Message = Enum.GetName(typeof(MotionType), currentmode);
 
-            this.Message = Enum.GetName(typeof(MotionType), currentmode);
+            this.CurrentMotionMode = MotionType.Linear;  // Init
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -66,117 +85,62 @@ namespace MachinaGrasshopper
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            //Machina.MotionType t = linear ? MotionType.Linear : MotionType.Joint;
-            DA.SetData(0, new ActionMotion(currentmode));
+            DA.SetData(0, new ActionMotion(CurrentMotionMode));
         }
 
-
+        /// <summary>
+        /// This is  regenerated every time the user right-clicks on the component: https://discourse.mcneel.com/t/manually-un-checking-additional-component-menu-items/54309/2
+        /// </summary>
+        /// <param name="menu"></param>
         protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
         {
             base.AppendAdditionalComponentMenuItems(menu);
 
-            //var lin = Menu_AppendItem(menu, "Linear", ModeToggle, null, true, mtToggles[MotionType.Linear]);
-            //lin.ToolTipText = "Straight linear motions in euclidean space.";
-            //var jnt = Menu_AppendItem(menu, "Joint", ModeToggle, null, true, mtToggles[MotionType.Joint]);
-            //jnt.ToolTipText = "Smooth joint interpolation between axes angles. NOTE: \"joint\" motion may produce unexpected trajectories resulting in reorientations or collisions; use with caution!";
+            //// This would be ideal, but need a way to implement description text programmatically in Core...
+            //Type mt = typeof(MotionType);
+            //foreach (MotionType type in Enum.GetValues(mt))
+            //{
+            //    var item = Menu_AppendItem(menu, Enum.GetName(mt, type), ModeToggle, true, mtToggles[type]);
+            //    item.ToolTipText = Enum.GetName(mt, type);
+            //    item.Tag = type;  // store the type as data tag
+            //}
 
-            var it = 0;
-            foreach (var name in Enum.GetNames(typeof(MotionType)))
-            {
-                var item = Menu_AppendItem(menu, name, ModeToggle, true, toggles[it++]);
-                item.ToolTipText = "Item " + (it - 1);
-            }
+            var lin = Menu_AppendItem(menu, "Linear", ModeToggle, null, true, mtToggles[MotionType.Linear]);
+            lin.ToolTipText = "Straight linear motions in euclidean space.";
+            lin.Tag = MotionType.Linear;
 
+            var jnt = Menu_AppendItem(menu, "Joint", ModeToggle, null, true, mtToggles[MotionType.Joint]);
+            jnt.ToolTipText = "Smooth joint interpolation between axes angles. NOTE: \"joint\" motion may produce unexpected trajectories resulting in reorientations or collisions; use with caution!";
+            jnt.Tag = MotionType.Joint;
         }
 
 
         protected void ModeToggle(Object sender, EventArgs e)
         {
-            var msgs = new List<string>();
-
             ToolStripMenuItem item = sender as ToolStripMenuItem;
             if (item == null) return;
-            this.RecordUndoEvent("currentMode");
 
-            msgs.Add(toggles[0] + " " + toggles[1] + " " + toggles[2]);
+            // Save some computing
+            if ((MotionType)item.Tag == CurrentMotionMode) return;
+            
+            // Record undo Mode
+            this.RecordUndoEvent("currentMode");  // Improve this: undo resets message, but not menu
 
-            for (var i = 0; i < toggles.Length; i++) toggles[i] = false;
-
-            var it = 0;
-            foreach (var name in Enum.GetNames(typeof(MotionType)))
-            {
-                if (name.Equals(item.Text))
-                {
-                    toggles[it] = true;
-                    msgs.Add("Changed " + name);
-                }
-                it++;
-            }
-
-
-
-
-
-                //foreach (var toggle in mtToggles)
-                //{
-                //    if (toggle.Value == true)
-                //    {
-                //        currentmode = toggle.Key;
-                //        break;
-                //    }
-                //}
-
-
-                //foreach (ToolStripMenuItem i in item.Owner.Items)
-                //{
-                //    i.Checked = false;
-                //}
-
-                //msgs.Add(item.Text + " prev state " + item.Checked);
-                //item.Checked = !item.Checked;
-                //msgs.Add(item.Text + " new state " + item.Checked);
-
-                //var items = item.Owner.Items;
-                ////foreach (var i in items.)
-
-                //foreach (var otherItems in (from object i in item.Owner.Items.GetEnumerator()
-                //                            let otherItems = i as ToolStripMenuItem
-                //                            where otherItems != null
-                //                            where i.Equals(item)
-                //                            select i)) 
-                //{
-                //    //otherItem
-                //}
-
-                //this.AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, currentmode.ToString());
-
-                //this.Message = linear ? "Linear" : "Joint";
-            this.Message = item.Text;
-            this.ExpireSolution(true);
-
-            foreach (var s in msgs)
-            {
-                this.AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, s);
-            }
+            // The setter will take care of updating the menus
+            this.CurrentMotionMode = (MotionType)item.Tag;
         }
 
         public override bool Write(GH_IWriter writer)
         {
-            //writer.SetBoolean("linear", linear);
-            writer.SetInt32("MotionMode", (int)currentmode);
+            writer.SetInt32("MotionMode", (int)CurrentMotionMode);
             return base.Write(writer);
         }
 
         public override bool Read(GH_IReader reader)
         {
-            //mType = reader.GetInt32("MotionType");
-            //linear = reader.GetBoolean("linear");
-            currentmode = (MotionType)reader.GetInt32("MotionMode");
-            this.Message = Enum.GetName(typeof(MotionType), currentmode);
-            this.ExpireSolution(true);
+            CurrentMotionMode = (MotionType)reader.GetInt32("MotionMode");
             return base.Read(reader);
         }
 
-        
     }
 }

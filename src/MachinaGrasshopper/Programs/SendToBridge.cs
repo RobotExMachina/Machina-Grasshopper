ad9785533fb6f7db6b3633280fa78a3ee59013f1
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-05-09 02:57:02 -0400

    # v0.6.2
    ## BUILD 1403
    
    ## BUILD 1402
    - [x] Add `ToInstruction()` override to Action:
        Generates a string representing a "serialized" instruction representing the Machina-API command that would have generated this action. Useful for generating actions to send to the Bridge.
    
    ---
    # v0.6.1
    
    ## BUILD 1401
    - [x] `Speed/To` can now be a `double`
    - [x] `Precision/To` can now be a `double`
    - [x] Tweaks to ABB compiler to accept the above.
    - [x] Add `Acceleration` and `AccelerationTo` actions: add to Actions, Cursor, Settings
      - [x] Add acceleration params to UR compiler
      - [x] Acceleration values of zero or less reset it back to the robot's default.
    - [x] Add `RotationSpeed/To()` option
      - [x] Add ABB correct compilation with defaults
      - [x] Add UR warning compilation message
    - [x] Add `JointSpeed/To()` and `JointAcceleration/To()` for UR robots
      - [x] Add UR compilation
      - [x] Add ABB compilation warnings
    - [x] UR streaming

---
+++ src/MachinaGrasshopper/Programs/SendToBridge.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grasshopper.Kernel;
+using Machina;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Net.WebSockets;
+using WebSocketSharp.Server;
+
+namespace MachinaGrasshopper.Programs
+{
+    public class SendToBridge : GH_Component
+    {
+        //  ███████╗███████╗███╗   ██╗██████╗ ████████╗ ██████╗ ██████╗ ██████╗ ██╗██████╗  ██████╗ ███████╗
+        //  ██╔════╝██╔════╝████╗  ██║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗██╔══██╗██║██╔══██╗██╔════╝ ██╔════╝
+        //  ███████╗█████╗  ██╔██╗ ██║██║  ██║   ██║   ██║   ██║██████╔╝██████╔╝██║██║  ██║██║  ███╗█████╗  
+        //  ╚════██║██╔══╝  ██║╚██╗██║██║  ██║   ██║   ██║   ██║██╔══██╗██╔══██╗██║██║  ██║██║   ██║██╔══╝  
+        //  ███████║███████╗██║ ╚████║██████╔╝   ██║   ╚██████╔╝██████╔╝██║  ██║██║██████╔╝╚██████╔╝███████╗
+        //  ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝    ╚═╝    ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝ ╚══════╝
+        //                                                       
+
+        private bool _sentOnce = false;
+                                                       
+        public SendToBridge() : base(
+            "SendToBridge",
+            "SendToBridge",
+            "Send a list of Actions to the Machina Bridge App to be streamed to a robot.",
+            "Machina",
+            "Programs")
+        { }
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        public override Guid ComponentGuid => new Guid("4442d8a9-3b6e-4197-ad58-93caa6c83e3e");
+        protected override System.Drawing.Bitmap Icon => null;
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Actions", "A", "A program in the form of a list of Actions", GH_ParamAccess.list);
+            pManager.AddTextParameter("BridgeURL", "URL", "The URL of the Machina Bridge App.", GH_ParamAccess.item, "ws://127.0.0.1:6999/Bridge");
+            pManager.AddBooleanParameter("Send", "S", "Send Actions?", GH_ParamAccess.item, false);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Instructions", "I", "Streamed Instructions", GH_ParamAccess.list);
+            pManager.AddTextParameter("Sent?", "ok", "Correctly sent?", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            
+            List<Machina.Action> actions = new List<Machina.Action>();
+            string url = "";
+            bool send = false;
+
+            if (!DA.GetDataList(0, actions)) return;
+            if (!DA.GetData(1, ref url)) return;
+            if (!DA.GetData(2, ref send)) return;
+
+            List<string> instructions = new List<string>();
+            if (send)
+            {
+                
+                string ins = "";
+                using (var ws = new WebSocket(url))
+                {
+                    ws.Connect();
+
+                    foreach (Machina.Action a in actions)
+                    {
+                        ins = a.ToInstruction();
+                        instructions.Add(ins);
+                        ws.Send(ins);
+                    }
+                    DA.SetData(1, "Sent!");
+                }
+            }
+            else
+            {
+                DA.SetData(1, "Nothing sent");
+            }
+            DA.SetDataList(0, instructions);
+
+        }
+    }
+}
